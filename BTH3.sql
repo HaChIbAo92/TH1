3148b2cc616ed5c535a20d77741de9cb923a80a5
HaChIbAo92 <118881798+HaChIbAo92@users.noreply.github.com>
2023-02-15 11:18:35 +0700

    Add files via upload

---
+++ BTH3.sql
@@ -0,0 +1,132 @@
+﻿--tạo db
+create database MarkManagement
+go 
+use MarkManagement
+go
+
+
+--tạo bảng
+create table Students
+(
+StudentID Nvarchar(12) Primary key,
+StudentName Nvarchar(25) Not null,
+DateofBirth Datetime Not null,
+Email Nvarchar(40),
+Phone Nvarchar(12),
+Class Nvarchar(10),
+)
+
+create table Subjects
+(
+SubjectID Nvarchar(10) Primary key,
+SubjectName Nvarchar(25) not null,
+)
+
+create table Mark
+(
+StudentID Nvarchar(12) FOREIGN KEY REFERENCES Students(StudentID),
+SubjectID Nvarchar(10) FOREIGN KEY REFERENCES Subjects(SubjectID),
+Datee Datetime,
+Theory Tinyint,
+Practical Tinyint,
+CONSTRAINT PK_Mark PRIMARY KEY (StudentID,SubjectID)
+)
+
+
+--Chèn dl vào bảng
+---students
+insert into Students
+values ('AV0807005', N'Mail Trung Hiếu', '11/10/1989', 'trunghieu@yahoo.com', '0904115116', 'AV1')
+insert into Students
+values ('AV0807006', N'Nguyễn Quý Hùng', '2/12/1988', 'quyhung@yahoo.com', '0955667787', 'AV2')
+insert into Students
+values ('AV0807007', N'Đỗ Đắc Huỳnh', '2/1/1990', 'dachuynh@yahoo.com', '0988574747', 'AV2')
+insert into Students
+values ('AV0807009', N'An Đăng Khuê', '6/3/1986', 'dangkhue@yahoo.com', '0986757463', 'AV1')
+insert into Students
+values ('AV0807010', N'Nguyễn T. Tuyết Lan', '12/7/1989', 'tuyetlan@yahoo.com', '0986757463', 'AV2')
+insert into Students
+values ('AV0807011', N'Đinh Phụng Long', '2/12/1990', 'phunglong@yahoo.com', '', 'AV1')
+insert into Students
+values ('AV0807012', N'Nguyễn Tuấn Nam', '2/3/1990', 'tuannam@yahoo.com', '', 'AV1')
+
+---Subjects
+insert into Subjects
+values ('S001', 'SQL')
+insert into Subjects
+values ('S002', 'Java Simplefield')
+insert into Subjects
+values ('S003', 'Acuve Server Page')
+
+---Mark
+insert into Mark
+values ('AV0807005', 'S001', '6/5/2008', 8, 25)
+insert into Mark
+values ('AV0807006', 'S002', '6/5/2008', 16, 30)
+insert into Mark
+values ('AV0807007', 'S001', '6/5/2008', 10, 25)
+insert into Mark
+values ('AV0807009', 'S003', '6/5/2008', 7, 13)
+insert into Mark
+values ('AV0807010', 'S003', '6/5/2008', 9, 16)
+insert into Mark
+values ('AV0807011', 'S002', '6/5/2008', 8, 30)
+insert into Mark
+values ('AV0807012', 'S001', '6/5/2008', 7, 31)
+insert into Mark
+values ('AV0807005', 'S002', '6/5/2008', 12, 11)
+insert into Mark
+values ('AV0807009', 'S002', '6/5/2008', 11, 20)
+insert into Mark
+values ('AV0807010', 'S001', '6/5/2008', 7, 6)
+
+
+--Thực hiện các truy vấn:
+--1. Hiển thị nội dung bảng Students
+select*from Students
+
+--2. Hiển thị nội dung danh sách sinh viên lớp AV1
+SELECT * FROM  Students
+WHERE class='AV1'
+
+--3. Sử dụng lệnh UPDATE để chuyển sinh viên có mã AV0807012 sang lớp AV2
+Select * from Students
+UPDATE Students
+Set Class = 'AV2'
+Where StudentID = 'AV0807012'
+
+--4. Tính tổng số sinh viên của từng lớp
+select Class, count(class) as "tổng số sv"
+from Students
+group by class
+
+--5. Hiển thị danh sách sinh viên lớp AV2 được sắp xếp tăng dần theo StudentName
+SELECT StudentName
+from Students
+Where Class = 'AV2'
+ORDER BY Class ASC;
+
+-- 6. Hiển thị danh sách sinh viên không đạt lý thuyết môn S001 (theory <10) thi ngày 6/5/2008
+select * from Students inner join Mark 
+ON Students.StudentID = Mark.StudentID
+Where SubjectID = 'S001' and theory < 10 and Datee = '6/5/2008'
+
+-- 7. Hiển thị tổng số sinh viên không đạt lý thuyết môn S001. (theory <10)
+select count(Class) As 'Tổng số sinh viên' 
+From Students inner join Mark 
+ON Students.StudentID = Mark.StudentID
+Where SubjectID = 'S001' and theory < 10
+
+-- 8.Hiển thị Danh sách sinh viên học lớp AV1 và sinh sau ngày 1/1/1980
+select * from Students 
+Where Class = 'AV1' and DateofBirth > '1/1/1980'
+
+-- 9. Xoá sinh viên có mã AV0807011
+DELETE FROM Students 
+Where StudentID = 'AV0807011';
+
+-- 10. Hiển thị danh sách sinh viên dự thi môn có mã S001 ngày 6/5/2008 bao gồm các trường sau: StudentID, StudentName, SubjectName, Theory, Practical, Date
+select Students.StudentID,Mark.SubjectID,Theory,Practical,Datee 
+from Students inner join Mark 
+ON Students.StudentID = Mark.StudentID 
+where SubjectID = 'S001' and Datee = '6/5/2008 '
---
+++ Sales.sql
@@ -0,0 +1,140 @@
+﻿SE master
+CREATE DATABASE Sales
+-- ON
+-- PRIMARY
+-- (
+-- NAME = tuan1_data,
+-- FILENAME =&#39;T:\ThucHanhSQL\tuan1_data.mdf&#39;,
+-- SIZE = 10MB,
+-- MAXSIZE = 20MB,
+-- FILEGROWTH = 20%
+-- )
+-- LOG ON
+-- (
+-- NAME = tuan1_log,
+-- FILENAME = &#39;T:\ThucHanhSQL\tuan1_log.ldf&#39;,
+-- SIZE = 10MB,
+-- MAXSIZE = 20MB,
+-- FILEGROWTH = 20%
+-- )
+USE Sales
+-- 1. Kiểu dữ liệu tự định nghĩa
+EXEC sp_addtype 'Mota','NVARCHAR(40)';
+EXEC sp_addtype 'IDKH', 'CHAR(10)', 'NOT NULL'
+EXEC sp_addtype 'DT', 'CHAR(12)';
+-- 2. Tạo table
+CREATE TABLE SanPham (
+MaSP CHAR(6) NOT NULL,
+TenSP VARCHAR(20),
+NgayNhap Date,
+DVT CHAR(10),
+SoLuongTon INT,
+DonGiaNhap money,
+)
+CREATE TABLE HoaDon (
+MaHD CHAR(10) NOT NULL,
+NgayLap Date,
+NgayGiao Date,
+MaKH IDKH,
+DienGiai Mota,
+)
+CREATE TABLE KhachHang (
+MaKH IDKH,
+TenKH NVARCHAR(30),
+DiaCHi NVARCHAR(40),
+DienThoai DT,
+)
+CREATE TABLE ChiTietHD (
+MaHD CHAR(10) NOT NULL,
+MaSP CHAR(6) NOT NULL,
+SoLuong INT
+)
+
+-- 3. Trong Table HoaDon, sửa cột DienGiai thành nvarchar(100).
+ALTER TABLE HoaDon
+ALTER COLUMN DienGiai NVARCHAR(100)
+
+-- 4. Thêm vào bảng SanPham cột TyLeHoaHong float
+ALTER TABLE SanPham
+ADD TyLeHoaHong float
+
+-- 5. Xóa cột NgayNhap trong bảng SanPham
+ALTER TABLE SanPham
+DROP COLUMN NgayNhap
+
+--6. Tạo các ràng buộc khóa chính và khóa ngoại cho các bảng trên
+ALTER TABLE SanPham ADD CONSTRAINT PK_SanPham PRIMARY KEY (MaSP);
+ALTER TABLE HoaDon ADD CONSTRAINT PK_HoaDon PRIMARY KEY (MaHD);
+ALTER TABLE KhachHang ADD CONSTRAINT PK_KhachHang PRIMARY KEY (MaKH);
+ALTER TABLE ChiTietHD ADD CONSTRAINT FK_ChiTietHoaDon FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD);
+ALTER TABLE ChiTietHD ADD CONSTRAINT FK_ChiTietSanPham FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP);
+ALTER TABLE ChiTietHD ADD CONSTRAINT PK_ChiTietHD PRIMARY KEY (MaHD,MaSP);
+ALTER TABLE HoaDon ADD CONSTRAINT FK_HoaDonKhachHang FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH);
+
+--7. Thêm vào bảng HoaDon các ràng buộc sau:
+---NgayGiao >= NgayLap
+ALTER TABLE HoaDon ADD CONSTRAINT CHECK_NgayGiao CHECK (NgayGiao>=NgayLap)
+---MaHD gồm 6 ký tự, 2 ký tự đầu là chữ, các ký tự còn lại là số
+ALTER TABLE HoaDon ADD CONSTRAINT chk_MaHD CHECK (MaHD LIKE '[a-zA-Z][a-zA-Z0-9][0-9][0-9][0-9][0-9]')
+---Giá trị mặc định ban đầu cho cột NgayLap luôn luôn là ngày hiện hành
+ALTER TABLE HoaDon ADD CONSTRAINT df_Ngay_Lap DEFAULT GETDATE() FOR NgayLap;
+
+--8. Thêm vào bảng Sản phẩm các ràng buộc sau:
+---SoLuongTon chỉ nhập từ 0 đến 500
+ALTER TABLE SanPham ADD CONSTRAINT CHECK_SoLuongTon CHECK(0<=SoLuongTon and SoLuongTon>=500)
+---DonGiaNhap lớn hơn 0
+ALTER TABLE SanPham ADD CONSTRAINT CHECK_DonGiaNhap CHECK(DonGiaNhap>0)
+---Giá trị mặc định cho NgayNhap là ngày hiện hành
+ALTER TABLE SanPham ADD NgayNhap date
+ALTER TABLE SanPham ADD CONSTRAINT df_Ngay_Nhap DEFAULT GETDATE() FOR NgayNhap;
+---DVT chỉ nhập vào các giá trị ‘KG’, ‘Thùng’, ‘Hộp’, ‘Cái’
+ALTER TABLE SanPham ADD CONSTRAINT CHECK_DVT CHECK(DVT='KG'OR DVT='THÙNG'OR DVT='HOP'OR DVT='CAI')
+
+--9. Dùng lệnh T-SQL nhập dữ liệu vào 4 table trên, dữ liệu tùy ý, chú ý các ràng buộc của mỗi Table
+--Sản Phẩm
+insert into SanPham (MaSP, TenSP, NgayNhap, DVT, SoLuongTon, DonGiaNhap)
+values ('A001', 'Bút bi', '10/02/2023', 'hộp', 200, '5000')
+insert into SanPham (MaSP, TenSP, NgayNhap, DVT, SoLuongTon, DonGiaNhap)
+values ('A002', 'Mì gói', '10/02/2023', 'thùng', 100, '120000')
+insert into SanPham (MaSP, TenSP, NgayNhap, DVT, SoLuongTon, DonGiaNhap)
+values ('A003', 'Đường', '10/02/2023', 'kg', 20, '20000')
+insert into SanPham (MaSP, TenSP, NgayNhap, DVT, SoLuongTon, DonGiaNhap)
+values ('A004', 'Vợt', '10/02/2023', 'cái', 10, '25000')
+--Hóa Đơn
+insert into HoaDon(MaHD, NgayLap, NgayGiao, MaKH, DienGiai)
+values ('AA01', '14/02/2023', '15/02/2023', 'KH01', 'Thanks')
+insert into HoaDon(MaHD, NgayLap, NgayGiao, MaKH, DienGiai)
+values ('AA02', '13/02/2023', '14/02/2023', 'KH02', 'Thanks')
+insert into HoaDon(MaHD, NgayLap, NgayGiao, MaKH, DienGiai)
+values ('AA03', '12/02/2023', '13/02/2023', 'KH03', 'Thanks')
+insert into HoaDon(MaHD, NgayLap, NgayGiao, MaKH, DienGiai)
+values ('AA04', '11/02/2023', '12/02/2023', 'KH04', 'Thanks')
+--Khách Hàng
+insert into KhachHang(MaKH, TenKH, DiaCHi, DienThoai)
+values ('KH01', N'Nguyễn Văn A', 'Phường 5, Tân Bình', '0123398645')
+insert into KhachHang(MaKH, TenKH, DiaCHi, DienThoai)
+values ('KH02', N'Trần Thị B', 'Phường 12, Tân Phú', '0936946235')
+insert into KhachHang(MaKH, TenKH, DiaCHi, DienThoai)
+values ('KH03', N'Võ Đông H', 'Bình Thạnh', '0936923235')
+insert into KhachHang(MaKH, TenKH, DiaCHi, DienThoai)
+values ('KH04', N'Nguyễn Minh T','Đồng Nai', '0936946125')
+--Chi tiết Hóa Đơn
+insert into ChiTietHD(MaHD, MaSP, SoLuong)
+values ('AA01', 'A001', 12)
+insert into ChiTietHD(MaHD, MaSP, SoLuong)
+values ('AA02', 'A002', 5)
+insert into ChiTietHD(MaHD, MaSP, SoLuong)
+values ('AA03', 'A003', 2)
+insert into ChiTietHD(MaHD, MaSP, SoLuong)
+values ('AA04', 'A004', 1)
+--10.Xóa 1 hóa đơn bất kỳ trong bảng HoaDon. Có xóa được không? Tại sao? Nếu vẫn muốn xóa thì phải dùng cách nào?
+
+
+--11.Nhập 2 bản ghi mới vào bảng ChiTietHD với MaHD = ‘HD999999999’ và MaHD=’1234567890’. Có nhập được không? Tại sao?
+
+
+--12.Đổi tên CSDL Sales thành BanHang
+
+
+--13.Tạo thư mục T:\QLBH, chép CSDL BanHang vào thư mục này, bạn có sao chép được không? Tại sao? Muốn sao chép được bạn phải làm gì? Sau khi sao chép bạn thực hiện Attach vào lại SQL.
+
